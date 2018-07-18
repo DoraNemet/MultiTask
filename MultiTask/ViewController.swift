75b537b9248c48e1b3785b6efe4a136eea49c0c3
Dora Fundak <dora.fundak@gmail.com>
2018-07-18 22:58:57 +0200

    Functional calculator

--- MultiTask/ViewController.swift
+++ MultiTask/ViewController.swift
@@ -9,7 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var numberOnScreen:Double = 0
+    var previousNumber:Double = 0
+    var preformingOperation = false
+    var operation = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,6 +24,72 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: false)
+    }
+    
+    @IBOutlet weak var currentLabel: UILabel!
+    @IBOutlet weak var lastNumberLabel: UILabel!
+    
+    @IBAction func numbers(_ sender: UIButton) {
+        if operation == 14 {
+            lastNumberLabel.text = currentLabel.text
+            currentLabel.text = ""
+            operation = 0
+            preformingOperation = false
+        }
+        if preformingOperation == true {
+            currentLabel.text = String(sender.tag)
+            numberOnScreen = Double(currentLabel.text!)!
+            preformingOperation = false
+        } else {
+            currentLabel.text = currentLabel.text! + String(sender.tag)
+            numberOnScreen = Double(currentLabel.text!)!
+        }
+    }
+    
+    @IBAction func buttons(_ sender: UIButton) {
+        if sender.tag == 14 { // =
+            lastNumberLabel.text = currentLabel.text
+            if operation == 10 {
+                currentLabel.text = String(previousNumber / numberOnScreen)
+            } else if operation == 11 {
+                currentLabel.text = String(previousNumber * numberOnScreen)
+            } else if operation == 12 {
+                currentLabel.text = String(previousNumber - numberOnScreen)
+            } else if operation == 13 {
+                currentLabel.text = String(previousNumber + numberOnScreen)
+            }
+            operation = 14
+        } else if sender.tag == 15 { //clear
+            lastNumberLabel.text = ""
+            currentLabel.text = ""
+            numberOnScreen = 0
+            preformingOperation = false
+            previousNumber = 0
+            operation = 0
+        } else if currentLabel.text != "" {
+            if  currentLabel.text != "/" && currentLabel.text != "+" && currentLabel.text != "x" && currentLabel.text != "-" {
+                previousNumber = Double(currentLabel.text!)!
+                lastNumberLabel.text = currentLabel.text
+            }
+            
+            if sender.tag == 10 { // /
+                currentLabel.text = "/"
+            } else if sender.tag == 11 { // x
+                currentLabel.text = "x"
+            } else if sender.tag == 12 {// -
+                currentLabel.text = "-"
+            } else if sender.tag == 13 {// +
+                currentLabel.text = "+"
+            }
+            operation = sender.tag
+            preformingOperation = true;
+        }
+    }
+    
 
 
 }
