dbabaa9f5e204974b3b2ba8f2c72e4bc0b3ad485
Dora Fundak <dora.fundak@gmail.com>
2018-07-18 16:02:39 +0200

    Initial Commit

---
+++ MultiTask/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MultiTask
+//
+//  Created by Dora Fundak on 18/07/2018.
+//  Copyright © 2018 Dora Fundak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
